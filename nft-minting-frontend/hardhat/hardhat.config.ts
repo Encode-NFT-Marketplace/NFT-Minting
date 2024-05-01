6011d9b139f510290a652491b3adfbdde5cb28ff
alireza-sharifpour <ascode94@gmail.com>
2024-05-01 20:26:00 +0330

    create contract

--- nft-minting-frontend/hardhat/hardhat.config.ts
+++ nft-minting-frontend/hardhat/hardhat.config.ts
@@ -3,6 +3,9 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  paths: {
+    artifacts: "./src/artifacts",
+  },
 };
 
 export default config;
